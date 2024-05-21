33b9bd8deb125eb490926d3ce78375dfee79e79f
joohyunje <joohj8677@naver.com>
2024-05-21 21:51:19 +0900

    5월 21일 todoList 리팩토링 시작

---
+++ workspace/5_vue-app2/1_prop-test/src/components/CheckboxItem4.vue
@@ -0,0 +1,18 @@
+<template>
+    <input type="checkbox" :checked="user.checked">{{user.number}}{{user.name}}
+    <br>
+</template>
+<script>
+
+import User from '../user.js'
+
+    export default {
+        name : 'CheckboxItem4',
+        props : {
+            user : User
+        }
+    }
+</script>
+<style lang="">
+    
+</style>
---
+++ workspace/5_vue-app2/1_prop-test/src/user.js
@@ -0,0 +1,10 @@
+export default class User {
+
+    constructor(number, name, checked){
+        this.number = number;
+        this.name = name;
+        this. checked = checked;
+
+    }
+
+}
